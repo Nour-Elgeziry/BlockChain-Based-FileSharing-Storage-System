8995c195ad55edaebc38af5d416c0bcfb9e9f04e
Nour-Elgeziry <nourahmedelgeziry@gmail.com>
2020-02-21 11:40:41 +0000

    ipfs connection

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,56 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import React, { Component } from "react";
+import logo from "../logo.png";
+import "./App.css";
+
+const ipfsClient = require("ipfs-http-client");
+const ipfs = ipfsClient({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https"
+}); // leaving out the arguments will default to these values
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buffer: null,
+      ipfsHash: ""
+    };
+  }
+  captureFile = event => {
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) });
+    };
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    console.log("Submitting file to ipfs...");
+    //1. upload file to ipfs
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log("Ipfs result", result);
+      if (error) {
+        console.error(error);
+        return;
+      }
+      //2. store file in block chain
+    });
+  };
   render() {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="https://github.com/Nour-Elgeziry/SSSDaap.git"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            SSSDapp
           </a>
         </nav>
         <div className="container-fluid mt-5">
@@ -25,20 +62,16 @@ class App extends Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
+                  <img
+                    src={`https://ipfs.infura.io/ipfs/${this.state.ipfsHash}`}
+                  />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                <p>&nbsp;</p>
+                <h2>Change File</h2>
+                <form onSubmit={this.onSubmit}>
+                  <input type="file" onChange={this.captureFile} />
+                  <input type="submit" />
+                </form>
               </div>
             </main>
           </div>
